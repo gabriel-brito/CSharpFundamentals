5eb55fd33884ee7cb126d81e236ab92b1ffec92d
Gabriel Santos <mfkrgabriel@gmail.com>
2023-02-18 12:21:29 +0200

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // byte number = 255;
+            // int count = 10;
+            // float totalPrice = 20.95f;
+            // char character = 'A';
+            // string firstName = "Gabriel";
+            // bool isLearning = true;
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Gabriel";
+            var isLearning = true;
+            
+            // The keyword "var" identifies the type withtout the need to explic it.
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isLearning);
+
+            // --- //
+
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", number, count, totalPrice, character, firstName, isLearning);
+        }
+    }
+}
