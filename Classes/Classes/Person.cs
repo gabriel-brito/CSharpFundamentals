b5ea8f986689bb07756f535d56d25bb407b39da8
Gabriel Santos <mfkrgabriel@gmail.com>
2023-03-04 14:29:10 +0200

    Classes

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person 
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, I am {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simplified way
+            // var person = new Person
+            // {
+            //     firstName = "Gabriel",
+            //     lastName = "Santos"
+            // };
+
+            var person = new Person();
+            person.firstName = "Gabriel";
+            person.lastName = "Santos";
+
+            person.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(5, 5);
+
+            Console.WriteLine(result);
+        }
+    }
+}
